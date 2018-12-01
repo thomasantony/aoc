55e0df20aed1b8f96f0ea3370896e6a7e333c7af
Thomas Antony <tantony@purdue.edu>
2018-12-01 16:27:05 -0600

    Switched to hashtable for faster runtime

--- nim/day_01/chronal_p2.nim
+++ nim/day_01/chronal_p2.nim
@@ -1,20 +1,24 @@
 from strutils import parseInt
 from itertools import cycle  #  nimble install itertools
+import tables
 
 var freq_list: seq[int]
 var total: int
 var all_lines: seq[string]
+var freq_map = initCountTable[int]()
 
 for line in stdin.lines:
   let number:int = parseInt(line)
   total = total + number
   all_lines.add(line)
-  freq_list.add(total)
+  freq_map.inc(total)
+  # freq_list.add(total)
 
 # Loop through list infinite number of times until repeated frequency is found
 for line in all_lines.cycle:
   let number:int = parseInt(line)
   total = total + number
-  if total in freq_list:
-    echo "Answer is ", total
-    break
+  # if total in freq_list:
+  if freq_map.hasKey(total):
+      echo "Answer is ", total
+      break
