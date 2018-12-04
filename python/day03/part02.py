4c6ac4fd6b2b12ae87db8d6af5dacb3ce81c4b9a
Thomas Antony <tantony@purdue.edu>
2018-12-03 19:12:37 -0600

    Removed more irrelevant variables

--- python/day03/part02.py
+++ python/day03/part02.py
@@ -14,11 +14,11 @@ def all_cells(left, top, w, h):
     return it.product(range(left, left+w), range(top, top+h))
 
 
-claim_ids = {id: (left, top, w, h) for id, left, top, w, h in map(parse_line, sys.stdin)}
-claim_counter = cl.Counter((x, y) for _, claim in claim_ids.items()
-                           for x, y in all_cells(*claim))
+claim_ids = {id: claim for id, *claim in map(parse_line, sys.stdin)}
+claim_counter = cl.Counter(cell for _, claim in claim_ids.items()
+                           for cell in all_cells(*claim))
 
 good_claim_id = next(id for id, claim in claim_ids.items()
-                     if all(claim_counter[(x, y)] == 1 for x, y in all_cells(*claim)))
+                     if all(claim_counter[cell] == 1 for cell in all_cells(*claim)))
 
 print('Good claim id', good_claim_id)
