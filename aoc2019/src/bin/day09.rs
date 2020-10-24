00067124b9615c5488a0909bae1c8496591150bc
Thomas Antony <tantony@purdue.edu>
2020-10-24 17:19:18 -0500

    Solved day 09 part A

--- aoc2019/src/bin/day09.rs
+++ aoc2019/src/bin/day09.rs
@@ -1,6 +1,13 @@
 use ::aoc2019::intcode::*;
+use ::aoc2019::{parse_numbers_with_delimiter, read_stdin};
 
 fn main()
 {
-    
+    let input = read_stdin();
+    let data: Vec<i64> = parse_numbers_with_delimiter(&input, ',').collect();
+
+    let mut vm = IntComputer::new();
+    vm.set_ram_size(2048);
+    let output_a = vm.load_program(&data).push_input(1).execute();
+    println!("Part A: {}", output_a[0]);
 }
