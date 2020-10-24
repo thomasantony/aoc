8697b746333afcec926c056bdbb727b03a444239
Thomas Antony <tantony@purdue.edu>
2020-10-24 17:19:04 -0500

    Added support for relative offset parameters and custom RAM size

--- aoc2019/src/intcode.rs
+++ aoc2019/src/intcode.rs
@@ -319,9 +319,9 @@ fn equal_op(vm: &mut IntComputer, parameters: Vec<Parameter>) {
     vm.ip += 4;
 }
 fn relative_base_offset_op(vm: &mut IntComputer, parameters: Vec<Parameter>) {
-    assert!(matches!(parameters[0], Parameter::Immediate(_)));
     let param1 = parameters[0].read(&vm.memory, vm.relative_base);
     vm.relative_base = ((vm.relative_base as i64) + param1) as usize;
+    vm.ip += 2;
 }
 
 fn halt_op(vm: &mut IntComputer, _: Vec<Parameter>) {
@@ -368,17 +368,24 @@ mod tests {
     fn unit_tests_day_09()
     {
         let mut vm = IntComputer::new();
+        
+        vm.set_ram_size(512);
+        assert_eq!(
+            vm.load_program(&vec![109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99])
+                .execute(),
+            vec![109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+        );
+
         assert_eq!(
             vm.load_program(&vec![104,1125899906842624,99])
                 .execute(),
                 vec![1125899906842624]
         );
-        // let mut vm = IntComputer::new();
-        // vm.set_ram_size(512);
-        // assert_eq!(
-        //     vm.load_program(&vec![109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99])
-        //         .execute(),
-        //     vec![109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
-        // )
+
+        assert_eq!(
+            vm.load_program(& vec![1102,34915192,34915192,7,4,7,99,0])
+            .execute(),
+            vec![1219070632396864]
+        );
     }
 }
