c56fa75438a0c05a0a68d2ecb2ba42e604c664be
Thomas Antony <tantony@purdue.edu>
2020-10-24 20:24:01 -0500

    Refactored in prep for day 10 part B

--- aoc2019/src/bin/day10.rs
+++ aoc2019/src/bin/day10.rs
@@ -7,12 +7,8 @@ use itertools::Itertools;
 
 type Point = (usize, usize);
 type Gradient = (i32, i32);
-fn approx_equal(a: f32, b: f32, decimal_places: u8) -> bool {
-    let factor = 10.0f32.powi(decimal_places as i32);
-    let a = (a * factor).trunc();
-    let b = (b * factor).trunc();
-    a == b
-}
+type GradMap = HashMap<Gradient, Vec<Point>>;
+
 fn read_asteroid_map(input: &String) -> Vec<Point>
 {
     let lines = get_lines(input);
@@ -28,15 +24,6 @@ fn read_asteroid_map(input: &String) -> Vec<Point>
     }
     points
 }
-fn find_visible_points(point: &(i32, i32), asteroid_map: &Vec<(i32, i32)>) -> Vec<(i32, i32)>
-{
-    for b in asteroid_map
-    {
-        let grad = (b.1-point.1, b.0-point.0);
-    }
-
-    Vec::new()
-}
 fn compute_gradient_and_distance(a: &Point, b: &Point) -> (Gradient, i32)
 {
     let grad = (a.1 as i32 - b.1 as i32 , a.0 as i32 - b.0 as i32);
@@ -48,44 +35,58 @@ fn compute_gradient_and_distance(a: &Point, b: &Point) -> (Gradient, i32)
         ((grad.0/gcd, grad.1/gcd), gcd)
     }else{
         (grad, 1)
-    }
-    
+    }   
 }
-fn solve_part_a(asteroid_map: &Vec<Point>) -> (Point, usize)
+/// Computes a HashMap with a gradient as key and vector of points sorted by distance as the value
+fn compute_gradient_map(point: &Point, asteroid_map: &Vec<Point>) -> GradMap
 {
-    let mut visible_count_map: HashMap<Point, usize> = HashMap::new();
+    let mut grad_map = HashMap::new();
+    for j in 0..asteroid_map.len()
+    {
+        let p2 = asteroid_map[j];
+        if point == &p2
+        {
+            continue;
+        }
+        let (gradient, dist) = compute_gradient_and_distance(&point, &p2);  
+        
+        let entry = grad_map.entry(gradient)
+                  .or_insert(Vec::new());
+        entry.push((dist, p2));
+    }
+    // Sort each vector by distance and remove the extra "distance" value in each item
+    let output = grad_map.into_iter().map(|(k, mut v)|{
+        v.sort();
+        let v = v.iter().map(|item| item.1).collect::<Vec<Point>>();
+        (k, v)
+    }).collect();
+    output
+}
+fn compute_visibility_map(asteroid_map: &Vec<Point>) -> HashMap<Point, GradMap>
+{
+    let visibility_map = HashMap::new();
     for i in 0..asteroid_map.len()
     {
         let p1 = asteroid_map[i];
-        let mut grad_map = HashMap::new();
-        for j in 0..asteroid_map.len()
-        {
-            if i == j{
-                continue;
-            }
-            let p2 = asteroid_map[j];
-            
-            let (gradient, dist) = compute_gradient_and_distance(&p1, &p2);  
-            
-            let entry = grad_map.entry(gradient)
-                                                                 .or_insert(Vec::new());
-            entry.push((dist, p2));
-        }
-        let num_visible_points = grad_map.values().count();
-        visible_count_map.insert(p1, num_visible_points);
+        let grad_map = compute_gradient_map(&p1, asteroid_map);
+        visibility_map.insert(p1, grad_map);
     }
-    let best_asteroid = visible_count_map
-                                                        .iter()
-                                                        .max_by_key(|(&k, &v)| v)
-                                                        .unwrap();
-    (*best_asteroid.0, *best_asteroid.1)
+    visibility_map
+}
+fn solve_part_a(visibility_map: &HashMap<Point, GradMap>) -> (Point, usize)
+{
+    let best_asteroid = visibility_map
+                        .iter()
+                        .max_by_key(|(_, v)| v.len())
+                        .unwrap();
+    (*best_asteroid.0, best_asteroid.1.len())
 }
 fn main()
 {
     let input = read_stdin();
     let asteroid_map = read_asteroid_map(&input);
-
-    let part_a = solve_part_a(&asteroid_map);
+    let visibility_map = compute_visibility_map(&asteroid_map);
+    let part_a = solve_part_a(&visibility_map);
     println!("Part A: {:?} with {} visible", part_a.0, part_a.1);    
 }
 
