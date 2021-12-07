aee498adda2458b14f48091ea9972eb2e76f5753
Thomas Antony <mail@thomasantony.com>
2021-12-06 19:38:36 -0600

    Add Iterator impl for AoC 2017 Day 03
    - Also add vscode debug config

--- aoc2017/src/bin/day03.rs
+++ aoc2017/src/bin/day03.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, hash::Hash};
+
 /// Day 03 - Spiral Memory
 /// 
 /// Part 1
@@ -18,46 +20,92 @@
 /// 3 -> (0, 0) + (1, 0) + (0, 1) = (1, 1)
 /// 5 -> (0, 0) + (1, 0) + (0, 1) + (-2, 0) = (-1, 1)
 /// 
+/// Implementing as iterator: The steps are as follows with stepping in "i"
+/// 
+/// turn, step, turn, step, turn step step, turn step step, turn step step step, etc.
+/// Part 2
+/// 
+/// Start with "1" in (0, 0)
+/// Go around the spiral, and in each cell put the sum of neighboring cells.
+/// 
+/// Might be worth converting the spiral function into an iterator
 
-fn get_pos_for_index(target: i32) -> (i32, i32)
+
+struct Spiral
 {
-    let mut pos = [0, 0];
-    let mut delta = [0, -1]; // Start facing south (turn left to [1, 0])
+    delta: [i32; 2],
+    pos: [i32; 2],
+    i: i32,
+    step_size: i32,
+    sub_step: i32,
 
-    let mut i = 1;
-    let mut step_size = 1;
-
-    while i < target {
-        delta = [-delta[1], delta[0]]; // turn left
-        pos[0] = pos[0] + delta[0]*step_size;
-        pos[1] = pos[1] + delta[1]*step_size;
-        
-        i += step_size;
-        if i >= target
-        {
-            break;
-        }
-        delta = [-delta[1], delta[0]]; // turn left
-        pos[0] = pos[0] + delta[0]*step_size;
-        pos[1] = pos[1] + delta[1]*step_size;
-        i += step_size;
-        step_size += 1;
-    }
-    if i > target {
-        let extra = i - target;
-        if extra > 0
-        {
-            pos[0] = pos[0] - delta[0]*extra;
-            pos[1] = pos[1] - delta[1]*extra;
-            i -= extra;
+}
+impl Spiral {
+    pub fn new() -> Self {
+        Self {
+            delta: [0, -1],
+            pos: [0, 0],
+            i: 1,           // This is the index 
+            step_size: 0,   // This is the step size of current leg. Goes up as 1, 2, 3 etc.
+            sub_step: 0,    // this is counting up through each step, 0->2*step_size
         }
     }
-    (pos[0], pos[1])
+}
+
+impl Iterator for Spiral {
+    type Item=(i32, i32);
+    fn next(&mut self) -> Option<(i32, i32)>
+    {
+        // Special case for start (can probably be added to general case somehow)
+        if self.step_size == 0
+        {
+            self.step_size = 1;
+            self.i = 2;
+            self.sub_step = 0;
+            return Some((self.pos[0], self.pos[1]));
+        }
+        // Turn at beginning of a step, and halfway through
+        if self.sub_step == 0 || self.sub_step == self.step_size {
+            self.delta = [-self.delta[1], self.delta[0]]; // turn left
+        }
+
+        // Step
+        self.pos[0] = self.pos[0] + self.delta[0];
+        self.pos[1] = self.pos[1] + self.delta[1];
+        self.i += 1;
+        self.sub_step += 1;
+
+        // Increment step size
+        if self.sub_step == 2*self.step_size
+        {
+            self.step_size += 1;
+            self.sub_step = 0;
+        }
+        return Some((self.pos[0], self.pos[1]));
+    }
 }
 fn main() 
 {
+    let mut spiral = Spiral::new();
     let target = 277678;
-    
-    let target_pos = get_pos_for_index(target);
+    let target_pos = spiral.nth(target-1).unwrap();
     println!("Part 1: {}", target_pos.0.abs() + target_pos.1.abs());
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_spiral_generator() {
+        let mut spiral = Spiral::new();
+        assert_eq!(spiral.next(), Some((0, 0)));
+        assert_eq!(spiral.next(), Some((1, 0)));
+        assert_eq!(spiral.next(), Some((1, 1)));
+        assert_eq!(spiral.next(), Some((0, 1)));
+        assert_eq!(spiral.next(), Some((-1, 1)));
+        assert_eq!(spiral.next(), Some((-1, 0)));
+        assert_eq!(spiral.next(), Some((-1, -1)));
+        assert_eq!(spiral.next(), Some(( 0, -1)));
+        assert_eq!(spiral.next(), Some(( 1, -1)));
+        assert_eq!(spiral.next(), Some(( 2, -1)));
+    }
 }
