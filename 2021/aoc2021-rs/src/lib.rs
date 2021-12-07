a784351f39ad0ddbe922de4a92e7d04fa4d9106a
Thomas Antony <mail@thomasantony.com>
2021-12-06 23:38:29 -0600

    Update AoC 2021 Day 07 comments + lib.rs

--- 2021/aoc2021-rs/src/lib.rs
+++ 2021/aoc2021-rs/src/lib.rs
@@ -1,1 +1,22 @@
+use std::str::FromStr;
+use std::fmt::Debug;
+pub fn parse_with_comma<T: FromStr>(input: &str) -> Vec<T>
+where <T as FromStr>::Err: Debug
+{
+     input
+        .trim()
+        .split(',')
+        .map(str::parse::<T>)
+        .map(Result::unwrap)
+        .collect::<Vec<_>>()
+}
 
+pub fn parse_numbers(input: &str) -> Vec<i32>
+{
+     input
+        .trim()
+        .split_ascii_whitespace()
+        .map(str::parse::<i32>)
+        .map(Result::unwrap)
+        .collect::<Vec<_>>()
+}
