900398e66178fe0ddcb0b44b07edfacf4bc4b0d5
Thomas Antony <mail@thomasantony.com>
2021-12-06 20:25:52 -0600

    Add solution for AoC 2017 Day 05

--- aoc2017/src/bin/day04.rs
+++ aoc2017/src/bin/day04.rs
@@ -1,18 +1,15 @@
 use std::collections::HashSet;
 
-
-fn is_valid_password_part01(passwd: &str) -> bool
-{
+fn is_valid_password_part01(passwd: &str) -> bool {
     let words = passwd.split(' ').collect::<Vec<_>>();
     let uniq = HashSet::<&str>::from_iter(words.iter().cloned());
     return uniq.len() == words.len();
 }
 
-fn is_valid_password_part02(passwd: &str) -> bool
-{
+fn is_valid_password_part02(passwd: &str) -> bool {
     let words = passwd.split(' ').collect::<Vec<_>>();
 
-    let words_with_sorted_chars = words.iter().map(|&w|{
+    let words_with_sorted_chars = words.iter().map(|&w| {
         let mut c = w.chars().collect::<Vec<_>>();
         c.sort();
         c.into_iter().collect::<String>()
@@ -21,17 +18,21 @@ fn is_valid_password_part02(passwd: &str) -> bool
     return uniq.len() == words.len();
 }
 
-fn main()
-{
+fn main() {
     let input = include_str!("../../inputs/day04.txt");
-    
+
     let passwords = input.trim().lines().collect::<Vec<_>>();
-    let part01_sol = passwords.iter().filter(|p|is_valid_password_part01(*p)).count();
+    let part01_sol = passwords
+        .iter()
+        .filter(|p| is_valid_password_part01(*p))
+        .count();
     println!("Part 1: {}", part01_sol);
 
-    let part02_sol = passwords.iter().filter(|p|is_valid_password_part02(*p)).count();
+    let part02_sol = passwords
+        .iter()
+        .filter(|p| is_valid_password_part02(*p))
+        .count();
     println!("Part 2: {}", part02_sol);
-
 }
 
 #[cfg(test)]
@@ -39,16 +40,16 @@ mod tests {
     use super::*;
     #[test]
     fn test_part_01() {
-        assert!( is_valid_password_part01("aa bb cc dd ee"));
-        assert!( !is_valid_password_part01("aa bb cc dd ee aa"));
-        assert!( is_valid_password_part01("aa bb cc dd aaa"));
+        assert!(is_valid_password_part01("aa bb cc dd ee"));
+        assert!(!is_valid_password_part01("aa bb cc dd ee aa"));
+        assert!(is_valid_password_part01("aa bb cc dd aaa"));
     }
     #[test]
     fn test_part_02() {
-        assert!( is_valid_password_part02("abcde fghij"));
-        assert!( !is_valid_password_part02("abcde xyz ecdab"));
-        assert!( is_valid_password_part02("a ab abc abd abf abj"));
-        assert!( is_valid_password_part02("iiii oiii ooii oooi oooo"));
-        assert!( !is_valid_password_part02("oiii ioii iioi iiio"));
+        assert!(is_valid_password_part02("abcde fghij"));
+        assert!(!is_valid_password_part02("abcde xyz ecdab"));
+        assert!(is_valid_password_part02("a ab abc abd abf abj"));
+        assert!(is_valid_password_part02("iiii oiii ooii oooi oooo"));
+        assert!(!is_valid_password_part02("oiii ioii iioi iiio"));
     }
 }
---
+++ aoc2017/src/bin/day05.rs
@@ -0,0 +1,67 @@
+fn parse_input(input: &str) -> Vec<i32> {
+    input
+        .trim()
+        .split_ascii_whitespace()
+        .map(str::parse::<i32>)
+        .map(Result::unwrap)
+        .collect::<Vec<_>>()
+}
+fn solve_part01(input: &str) -> i32 {
+    let mut jumps = parse_input(input);
+
+    let mut idx = 0i32;
+    let mut counter = 0;
+    while idx >= 0 {
+        if let Some(jump_dist) = jumps.get(idx as usize).cloned() {
+            jumps[idx as usize] += 1;
+            idx = idx + jump_dist;
+        } else {
+            break;
+        }
+        counter += 1;
+    }
+    counter
+}
+fn solve_part02(input: &str) -> i32 {
+    let mut jumps = parse_input(input);
+
+    let mut idx = 0i32;
+    let mut counter = 0;
+    while idx >= 0 {
+        if let Some(jump_dist) = jumps.get(idx as usize).cloned() {
+            if jump_dist >= 3 {
+                jumps[idx as usize] -= 1;
+            } else {
+                jumps[idx as usize] += 1;
+            }
+
+            idx = idx + jump_dist;
+        } else {
+            break;
+        }
+        counter += 1;
+    }
+    counter
+}
+fn main() {
+    let input = include_str!("../../inputs/day05.txt");
+
+    let part01_sol = solve_part01(input);
+    println!("Part 1: {}", part01_sol);
+
+    let part02_sol = solve_part02(input);
+    println!("Part 1: {}", part02_sol);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_part_01() {
+        assert_eq!(solve_part01("0 3 0 1 -3"), 5);
+    }
+    #[test]
+    fn test_part_02() {
+        assert_eq!(solve_part02("0 3 0 1 -3"), 10);
+    }
+}
